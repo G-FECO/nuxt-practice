84f1b3a8cc600ef5cc31c3487272aadb29dea5d8
wally-wally <wallys0213@gmail.com>
2020-10-23 15:41:26 +0900

    201023 | create nuxt-practice repository

---
+++ pages/buckets/_user.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-if="!isValid">유효하지 않는 주소입니다.</div>
+    <div v-else>{{ userId }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userParams: this.$route.params.user
+    }
+  },
+  computed: {
+    isValid() {
+      return this.userParams && this.userParams[0] === '@' && this.userParams.length >= 2
+    },
+    userId() {
+      return this.userParams.slice(1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <h2>메인 랜딩 페이지</h2>
+    <el-button type="primary" round @click="goBucketPage">
+      Log in with Google
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: 'wally'
+    }
+  },
+  methods: {
+    goBucketPage() {
+      this.$router.push(`/buckets/@${this.user}`);
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  width: 300px;
+  padding: 10px;
+  border: 1px solid $colorLight;
+  border-radius: 10px;
+  box-shadow: 2px 4px 4px $colorLight;
+  
+  > h2 {
+    color: $colorBlue;
+    margin-bottom: 10px;
+  }
+}
+</style>
